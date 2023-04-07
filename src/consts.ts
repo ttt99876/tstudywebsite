54a0bba3bec0711f902a9ee4638883fffa9690e6
物联小T <8539586+iot-small-t@user.noreply.gitee.com>
2023-04-07 10:28:50 +0800

    修改github路径指向

--- src/consts.ts
+++ src/consts.ts
@@ -19,9 +19,10 @@ export const KNOWN_LANGUAGES = {
 } as const;
 export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
-// export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/tree/main/examples/docs`;
+export const GITHUB_EDIT_URL = `https://github.com/ttt99876/tstudywebsite`;
 
-export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
+// 加入社区
+// export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
 
 // See "Algolia" section of the README for more information.
 export const ALGOLIA = {
